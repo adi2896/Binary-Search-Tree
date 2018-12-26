f298f96d2a485e18c9b63b9fbcd2748fce0d452a
Aditya Parandekar <37509528+adi2896@users.noreply.github.com>
2018-12-26 13:34:31 -0800

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,69 +1,49 @@
-#ifndef bst_h
-#define bst_h
-
-typedef struct data
+#ifndef BST_H
+#define BST_H
+typedef struct Data
 {
 	int value;
-}Data;
+} Data;
 
-typedef struct node
+typedef struct Node
 {
 	Data data;
-	struct node * left;
-	struct node * right;
-	struct node * parent;
-}Node;
+	struct Node *parent;
+	struct Node *left;
+	struct Node *right;
+} Node;
 
-typedef struct Tree
+typedef struct  Tree
 {
-	Node * root;
-	Data * (*insert)(struct Tree *, Data);
-	Data * (*search)(struct Tree * bst, Data value);
-	void (*sort)(struct Tree *, Data *);
-	int (*compare)(struct Tree *t, struct Tree * copy);
-	struct Tree * (*clone)(struct Tree *t);
+    Node *root;
+	Data * (*insert)(struct Tree *bst, Data data);
+	Data * (*search)(struct Tree * bst, Data data);
+	void (*sort)(struct Tree *bst, Data *data);
+	int (*compare)(struct Tree *tree,struct Tree *treeCopy);
+	struct Tree * (*clone)(struct Tree *tree);
 	void (*delete)(struct Tree * bst);
 	void (*removeData)(struct Tree * bst, Data value);
-}Tree;
-
-Node * newNode(Data, Node *);
+} Tree;
 
 Tree * newTree();
-
-Data * insert(Tree *, Data);
-
-Data * insertNode(Node *, Data);
-
-void sort(Tree *, Data *);
-
-int sortAux(Node *, Data *, int );
-
-Data * search(Tree *, Data);
-
-Node * searchTree(Tree *, Data);
-
-Node * searchNode(Node *, Data);
-
-Tree * clone(Tree *);
-
-Tree * traversen(Node *, Tree *);
-
-int compare(Tree *, Tree *);
-
-int compared(Node *, Node *);
-
-void removeData(Tree *, Data);
-
-void removeLeaf(Tree *, Node *);
-
-void shortCircuit(Tree *, Node *);
-
-void promotion(Tree *, Node *);
-
-void deleteTree(Tree *);
-
-void deleteData(Tree *, Node *);
-
-void inorder(Tree *, Node *);
+Data * insert(Tree *bst, Data data);
+Data * insertNode(Node *node, Data data);
+void sort(Tree *bst, Data *data);
+Data * search(Tree * bst, Data value);
+Node * searchNode(Node *node, Data data);
+Node * newNode(Data data, Node * parent);
+void sortTree(Node *node, Data *data, int *index);
+int compare(Tree *tree, Tree *treeCopy);
+int comparedTree(Node *, Node *);
+Tree * clone(Tree *tree);
+void delete(Tree * bst);
+void removeData(Tree * bst, Data data);
+void Traversal(Node *node, Data *data, int *index);
+Node * cloneNode(Node *originalNode, Node *node1, Node *parentNode);
+void removeLeaf(Node * leaf);
+void shortCircuit(Node *node);
+Node * searchMax(Node *node);
+void promotion(Node *node);
+void deleteTree(Node *node);
 
 #endif
